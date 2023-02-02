69cc0c47b8e2d631bcc728a1d0aa87f008bd6825
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2023-02-02 19:06:56 +0100

    starting on image gallery design

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import "./Gallery.css";
 import Gallery1 from "./Gallery1.png";
+import Golfo from "./Golfo.png";
+import School from "./School.mp4";
+import Gambarie from "./Gambarie.png";
+
+
 import { ImageViewer } from "react-image-viewer-dv"
 
 
@@ -17,48 +22,302 @@ const Gallery = () => {
 
 
             <div className="image-gallery">
+          <div className="photo">
+          <ImageViewer>
+           <div className="card1">
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+            </div>
+          </ImageViewer>
+          </div>
+          <div className="photo">
+          <div className="card2">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+            <div className="card3">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card4">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+
+          <div>
+          <div className="middle">
+          <ImageViewer>
+          <div className="middle">
+            <img src={Gambarie} height={400} alt="middle" />
+          </div>
+          </ImageViewer>
+          </div>
+          </div>
 
           <div className="photo">
-           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+          <div className="card5">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card6">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card7">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card8">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card9">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card10">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+
+
+
+
+
+
+          <div className="image-gallery2">
+          <div className="photo">
+          <ImageViewer>
+           <div className="card1">
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+            </div>
           </ImageViewer>
           </div>
           <div className="photo">
+          <div className="card2">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+            <div className="card3">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card4">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+
+          <div>
+          <div className="Golfo">
+          <ImageViewer>
+          <div className="Golfo">
+            <img src={Golfo} height={400} alt="Golfo" />
+          </div>
+          </ImageViewer>
+          </div>
+          </div>
+
+          <div className="photo">
+          <div className="card5">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card6">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card7">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card8">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card9">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card10">
+
+
+
+
+
+          <div className="image-gallery2">
+          <div className="photo">
+
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          <div className="photo">
+          <ImageViewer>
+           <div className="card1">
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+            </div>
           </ImageViewer>
           </div>
           <div className="photo">
+          <div className="card2">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
           </ImageViewer>
+          </div>
           </div>
           <div className="photo">
+            <div className="card3">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
           </ImageViewer>
+          </div>
           </div>
           <div className="photo">
+          <div className="card4">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
           </ImageViewer>
+          </div>
+          </div>
+
+          <div>
+          <div className="School">
+          <ImageViewer>
+          <div className="School">
+          <video src={School} autoPlay loop muted />
+          </div>
+          </ImageViewer>
+          </div>
+          </div>
+
+          <div className="photo">
+          <div className="card5">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
           </div>
           <div className="photo">
+          <div className="card6">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
           </ImageViewer>
+          </div>
           </div>
           <div className="photo">
+          <div className="card7">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
           </ImageViewer>
+          </div>
           </div>
           <div className="photo">
+          <div className="card8">
           <ImageViewer>
-            <img src={Gallery1} height={200} alt="" />
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
           </ImageViewer>
           </div>
           </div>
+          <div className="photo">
+          <div className="card9">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          <div className="photo">
+          <div className="card10">
+          <ImageViewer>
+            <img src={Gallery1} height={100} alt="" />
+            <div class="caption">I Miss London</div>
+          </ImageViewer>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+
+
+
+
 
 
 
