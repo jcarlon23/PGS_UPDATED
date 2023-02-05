69d036e44d7a29386712f4a6c0be53ab69843394
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2023-02-05 11:04:46 +0100

    template done for contact us

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import "./Contact.css";
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div class="container">
+	<div class="row">
+			<h9>Contattaci</h9>
+	</div>
+	<div class="row">
+			<h10>Ci piacerebbe sentirti!</h10>
+	</div>
+	<div class="row input-container">
+			<div class="col-xs-12">
+				<div class="styled-input wide">
+					<input type="text" required />
+					<label>Name</label>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<div class="styled-input">
+					<input type="text" required />
+					<label>Email</label>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<div class="styled-input">
+					<input type="text" required />
+					<label>Phone Number</label>
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<div class="styled-input wide">
+					<textarea required></textarea>
+					<label>Message</label>
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<div class="btn-lrg submit-btn">Send Message</div>
+			</div>
+	</div>
+</div>
+
   )
 }
 
