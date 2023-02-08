69a5b301fcf626924b6076467fd159c781f71cb6
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2023-02-09 00:03:32 +0100

    images added to gallery and footprints need to be placed correctly

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -7,6 +7,41 @@ import Gallery1 from "./Gallery1.png";
 import Golfo from "./Golfo.png";
 import School from "./School.mp4";
 import Gambarie from "./Gambarie.png";
+import Gallery2 from "./Gallery2.png";
+import Gallery3 from "./Gallery3.png";
+import Gallery4 from "./Gallery4.png";
+import Gallery5 from "./Gallery5.png";
+import Gallery6 from "./Gallery6.png";
+import Gallery7 from "./Gallery7.png";
+import Gallery8 from "./Gallery8.png";
+import Gallery9 from "./Gallery9.png";
+import Gallery10 from "./Gallery10.png";
+
+import Citta1 from "./Citta1.png";
+import Citta2 from "./Citta2.png";
+import Citta3 from "./Citta3.png";
+import Citta4 from "./Citta4.png";
+import Citta5 from "./Citta5.png";
+import Citta6 from "./Citta6.png";
+import Citta7 from "./Citta7.png";
+import Citta8 from "./Citta8.png";
+
+import Bosco1 from "./Bosco1.png";
+import Bosco2 from "./Bosco2.png";
+import Bosco3 from "./Bosco3.png";
+import Bosco4 from "./Bosco4.png";
+import Bosco5 from "./Bosco5.png";
+import Bosco6 from "./Bosco6.png";
+import Bosco7 from "./Bosco7.png";
+import Bosco8 from "./Bosco8.png";
+import Bosco9 from "./Bosco9.png";
+import Bosco10 from "./Bosco10.png";
+
+
+
+
+
+
 import foot from "./foot.png";
 import { ImageViewer } from "react-image-viewer-dv";
 
@@ -67,7 +102,7 @@ const Gallery = () => {
           <div className="photo">
           <ImageViewer>
            <div className="card1">
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery1} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
             </div>
           </ImageViewer>
@@ -75,7 +110,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card2">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery2} height={120} width={120}  alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -83,7 +118,7 @@ const Gallery = () => {
           <div className="photo">
             <div className="card3">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery3} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -91,7 +126,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card4">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery4} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -110,7 +145,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card5">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery5} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -118,7 +153,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card6">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery6} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -126,7 +161,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card7">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery7} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -134,7 +169,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card8">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery8} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -142,7 +177,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card9">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery9} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -150,7 +185,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card10">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Gallery10} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -185,18 +220,10 @@ const Gallery = () => {
 
 
         <div className="image-gallery2">
-        <div className="photo">
-          <ImageViewer>
-           <div className="card11">
-            <img src={Gallery1} height={100} alt="" />
-            <div className="caption">I Miss London</div>
-            </div>
-          </ImageViewer>
-          </div>
           <div className="photo">
           <div className="card12">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta2} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -204,7 +231,7 @@ const Gallery = () => {
           <div className="photo">
             <div className="card13">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta3} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -212,7 +239,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card14">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta4} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -231,7 +258,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card15">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta5} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -239,7 +266,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card16">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta6} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -247,7 +274,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card18">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta7} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -255,7 +282,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card19">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta8} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -263,7 +290,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card20">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Citta1} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -276,7 +303,7 @@ const Gallery = () => {
           <div className="photo">
           <ImageViewer>
            <div className="card21">
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco1} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
             </div>
           </ImageViewer>
@@ -284,7 +311,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card22">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco2} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -292,7 +319,7 @@ const Gallery = () => {
           <div className="photo">
             <div className="card23">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco3} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -300,7 +327,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card24">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco4} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -319,7 +346,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card25">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco5} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -327,7 +354,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card26">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco6} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -335,7 +362,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card27">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco7} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -343,7 +370,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card28">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco8} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -351,7 +378,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card29">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco9} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
@@ -359,7 +386,7 @@ const Gallery = () => {
           <div className="photo">
           <div className="card30">
           <ImageViewer>
-            <img src={Gallery1} height={100} alt="" />
+            <img src={Bosco10} height={120} width={120} alt="" />
             <div className="caption">I Miss London</div>
           </ImageViewer>
           </div>
