678c5c47cac1b1bdd21b646792b0548deff5e48b
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2023-05-01 22:24:42 +0200

    styles added to hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,24 +17,24 @@ const Navbar = () => {
         </Link>
 
         <div
-          className="hamburger sm:visible md:invisible "
+          className="hamburger  sm:visible md:invisible  "
           onClick={showMobileMenu}
         >
           {showMenuForMobile ? (
-            <FaTimes size={30} style={{ color: "#333" }} />
+            <FaTimes size={30} style={{ color: "black" }} />
           ) : (
-            <FaBars size={30} style={{ color: "#333" }} />
+            <FaBars size={30} style={{ color: "black" }} />
           )}
         </div>
 
         <ul
           className={
             showMenuForMobile
-              ? "nav-menu active md:flex md:flex-row "
-              : "menuhidden md:visible md:flex md:flex-row"
+              ? "nav-menu active flex flex-col md:flex md:flex-row "
+              : "menuhidden md:visible md:flex md:flex-row "
           }
         >
-          <li className="nav-item px-0.5 lg:px-3 py-1  text-xs md:text-m lg:text-lg font-medium text-black hover:underline underline-offset-8 ">
+          <li className="nav-item  hover-textShadow px-0.5 lg:px-3 py-1 text-xs md:text-m lg:text-lg font-medium md:text-black hover:underline underline-offset-8 ">
             <Link to="/" onClick={hideMobileMenu}>
               HOME
             </Link>
@@ -44,17 +44,17 @@ const Navbar = () => {
             CALENDARIO
             </Link>
           </li> */}
-          <li className="nav-item px-1.0 lg:px-3 py-1 text-xs md:text-m lg:text-lg font-medium text-black hover:underline underline-offset-8">
+          <li className="nav-item px-1.0 lg:px-3 py-1 text-xs md:text-m lg:text-lg font-medium md:text-black hover:underline underline-offset-8">
             <Link to="About" onClick={hideMobileMenu}>
               CHI SIAMO
             </Link>
           </li>
-          <li className="nav-item px-0.5 lg:px-3 py-1 text-xs	md:text-m lg:text-lg font-medium text-black hover:underline underline-offset-8">
+          <li className="nav-item px-0.5 lg:px-3 py-1 text-xs	md:text-m lg:text-lg font-medium md:text-black hover:underline underline-offset-8">
             <Link to="Gallery " onClick={hideMobileMenu}>
               GALLERIA
             </Link>
           </li>
-          <li className="nav-item  px-0.5  lg:px-3 py-1 text-xs	md:text-m lg:text-lg font-medium text-black hover:underline underline-offset-8 md:mr-10">
+          <li className="nav-item  px-0.5  lg:px-3 py-1 text-xs	md:text-m lg:text-lg font-medium md:text-black hover:underline underline-offset-8 md:mr-10">
             <Link to="ContactUs" onClick={hideMobileMenu}>
               CONTATTACI
             </Link>
